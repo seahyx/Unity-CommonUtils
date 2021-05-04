5078262ccfbdf91c89d0d823d728ef04da67c96b
Seah Ying Xiang <seahyx@gmail.com>
2021-05-04 21:13:31 +0800

    Added Interactable sprite theme engine workaround

---
+++ MRTK Interactable Sprite Theme Engine/InteractableWorkaroundSpriteTheme.cs
@@ -0,0 +1,88 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using CommonUtils;
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.UI
+{
+	/// <summary>
+	/// <para>
+	/// Theme Engine to change the <see cref="int"/> value on a <see cref="SpriteIndexer"/> component, which will change the sprite on the object.
+	/// This is a workaround for the inane fact that there is no <see cref="ThemeStateProperty"/> for <see cref="Sprite"/>s.
+	/// </para>
+	/// As this is a workaround, there may be visual bugs.
+	/// </summary>
+	public class InteractableWorkaroundSpriteTheme : InteractableThemeBase
+	{
+		/// <inheritdoc />
+		public override bool IsEasingSupported => false;
+
+		private SpriteIndexer spriteIndexer;
+
+		public InteractableWorkaroundSpriteTheme()
+		{
+			Types = new Type[] { typeof(SpriteIndexer) };
+			Name = "Workaround Sprite Theme";
+		}
+
+		/// <inheritdoc />
+		public override ThemeDefinition GetDefaultThemeDefinition()
+		{
+			return new ThemeDefinition()
+			{
+				ThemeType = GetType(),
+				StateProperties = new List<ThemeStateProperty>()
+				{
+					new ThemeStateProperty()
+					{
+						Name = "Sprite Index",
+						Type = ThemePropertyTypes.Int,
+						Values = new List<ThemePropertyValue>(),
+						Default = new ThemePropertyValue() { Int = 0 }
+					},
+				},
+				CustomProperties = new List<ThemeProperty>(),
+			};
+		}
+
+		/// <inheritdoc />
+		public override void Init(GameObject host, ThemeDefinition settings)
+		{
+			spriteIndexer = host.GetComponent<SpriteIndexer>();
+
+			base.Init(host, settings);
+		}
+
+		/// <inheritdoc />
+		public override ThemePropertyValue GetProperty(ThemeStateProperty property)
+		{
+			ThemePropertyValue start = new ThemePropertyValue();
+			start.Int = 0;
+
+			if (spriteIndexer != null)
+			{
+				start.Int = spriteIndexer.GetIndex();
+			}
+
+			return start;
+		}
+
+		/// <inheritdoc />
+		public override void SetValue(ThemeStateProperty property, int index, float percentage)
+		{
+			SetValue(property, property.Values[index]);
+		}
+
+		/// <inheritdoc />
+		protected override void SetValue(ThemeStateProperty property, ThemePropertyValue value)
+		{
+			if (spriteIndexer != null)
+			{
+				spriteIndexer.SetIndex(value.Int);
+			}
+		}
+	}
+}
---
+++ MRTK Interactable Sprite Theme Engine/SpriteIndexer.cs
@@ -0,0 +1,93 @@
+﻿/**
+ *	Common Utilities Library for developing in Unity.
+ *	https://github.com/seahyx/Unity-CommonUtils
+ *
+ *	MIT License
+ *
+ *	Copyright (c) 2021 Seah Ying Xiang
+ */
+
+using Sirenix.OdinInspector;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CommonUtils
+{
+	/// <summary>
+	/// Indexes a list of <see cref="Sprite"/>s and uses it to update a <see cref="SpriteRenderer"/> via the list index.
+	/// </summary>
+	[RequireComponent(typeof(SpriteRenderer))]
+	public class SpriteIndexer : MonoBehaviour
+	{
+		#region Serialization
+
+		[Title("Configuration")]
+
+		[Tooltip("Sprite list.")]
+		[SerializeField]
+		private List<Sprite> sprites = new List<Sprite>();
+
+		[Tooltip("Current index.")]
+		[PropertyRange(0, "maxSpriteIndex")]
+		[SerializeField]
+		private int currentIndex = 0;
+
+		#endregion
+
+		#region Member Declarations
+
+		/// <summary>
+		/// <see cref="SpriteRenderer"/> reference.
+		/// </summary>
+		private SpriteRenderer spriteRenderer;
+
+		/// <summary>
+		/// Maximum index of <see cref="sprites"/>.
+		/// </summary>
+		private int maxSpriteIndex => sprites?.Count - 1 ?? 0;
+
+		#endregion
+
+		#region MonoBehaviour
+
+		/// <summary>
+		/// Resource initialization.
+		/// </summary>
+		private void Awake()
+		{
+			spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+		}
+
+		/// <summary>
+		/// Update the <see cref="spriteRenderer"/>.
+		/// </summary>
+		private void OnEnable()
+		{
+			SetIndex(currentIndex);
+		}
+
+		#endregion
+
+		#region Public Functions
+
+		/// <summary>
+		/// Change the sprite on this GameObject by changing the index for the sprite to be displayed.
+		/// If the index is outside of the sprite list's range, it will be ignored.
+		/// </summary>
+		/// <param name="index">Index of the sprite in the list to change to.</param>
+		public void SetIndex(int index)
+		{
+			if (sprites != null)
+			{
+				if (index >= 0 && index <= maxSpriteIndex)
+				{
+					spriteRenderer.sprite = sprites[index];
+				}
+			}
+		}
+
+		public int GetIndex() => currentIndex;
+
+		#endregion
+	}
+}
